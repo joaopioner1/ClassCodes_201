d0bd43506c9250e94a2d5529e5242e1d32ec2173
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-06-16 15:01:15 -0300

    First commit/ add ex 1 (loops)

---
+++ src/loopsExcercises/Ex1.java
@@ -0,0 +1,39 @@
+package loopsExcercises;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import exceptions.NegativeNumberException;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		//Author: Joao Vitor Souza Pioner | Date: 16/06/2021
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter a positive number: ");
+		Integer number = in.nextInt();
+		checkNumber(number);
+		try {
+			for (int i = 0; i <= number; i++) {
+				if (i % 2 == 0) {
+					System.out.println(i);
+				}
+			}
+		}
+		catch (NullPointerException e) {
+			System.out.println("ERROR: " + e.getMessage());
+		}
+		catch (InputMismatchException e) {
+			System.out.println("ERROR: you must enter a positive number!");
+		}
+		
+		in.close();
+	}
+	
+	public static void checkNumber(Integer number) {
+		if (number < 0) {
+			throw new NegativeNumberException("THE NUMBER MUST BE POSITIVE!!!");
+		}
+	}
+}
