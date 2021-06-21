af2f22856a0504d10a70a4a777f155289c0e5756
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-06-21 09:27:05 -0300

    add class that contains all methods excercises

---
+++ src/function/AllMethodsExcercises.java
@@ -0,0 +1,11 @@
+package function;
+
+public class AllMethodsExcercises {
+
+	public static void main(String[] args) {
+		//Author: Joao Vitor Souza Pioner | Date: 21/06/2021
+		
+		
+	}
+
+}
--- src/loopsExcercises/Ex7.java
+++ src/loopsExcercises/Ex7.java
@@ -1,10 +1,38 @@
 package loopsExcercises;
 
+import java.util.Scanner;
+
 public class Ex7 {
 
 	public static void main(String[] args) {
 		// Author: Joao Vitor Souza Pioner | Date: 20/06/2021
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number of lines (1-19): ");
+		int lines = sc.nextInt();
+		
+		String string = "", str, teste = "";
+		String empty = "";
+		
+		for (int i = 1; i <= lines; i++) {
+			empty += " ";
+		}
+		
+		for (int i = 1; i <= lines; i++) {
+			string += "*";
+			
+			str = empty.substring(i);
+			System.out.println(str + string + teste);
+			teste += string;
+		}
+		
+		
+		/*for (int i = 1; i <= lines; i++) {
+			string += "*";
+			str = empty.substring(i);
+			System.out.println(str + string);
+		}*/
+		sc.close();
 	}
 
 }
